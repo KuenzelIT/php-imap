5e9d4ea36072865103328da89f2243b9ee37881b
KuenzelIT <kontakt@kuenzelit.de>
2015-11-05 13:37:48 +0100

    temporary fix for my imap_search

--- src/PhpImap/Mailbox.php
+++ src/PhpImap/Mailbox.php
@@ -181,7 +181,7 @@ class Mailbox {
 	 * @return array Mails ids
 	 */
 	public function searchMailbox($criteria = 'ALL') {
-		$mailsIds = imap_search($this->getImapStream(), $criteria, SE_UID, $this->serverEncoding);
+		$mailsIds = imap_search($this->getImapStream(), $criteria, SE_UID);
 		return $mailsIds ? $mailsIds : array();
 	}
 
